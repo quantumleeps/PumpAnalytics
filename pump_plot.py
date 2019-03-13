6db5144da8ce03a5e14cf83abc61555da7e15e85
quantumleeps <dan.leeper@gmail.com>
2019-03-13 03:59:07 +0000

    some good breaking updates to the plotter

--- pump_plot.py
+++ pump_plot.py
@@ -21,7 +21,9 @@ unit_mapper = {
     'meter**3/hour': 'Flowrate (m^3/hr)',
     'horsepower': 'Power (hp)',
     'kilowatt': 'Power (kW)',
-    'dimensionless': 'Efficiency'
+    'dimensionless': 'Efficiency',
+    'megawatt': 'Power (mW)',
+    'meter**3/second': 'Flowrate (m^3/s)'
 }
 
 def make_polyfit(x,y,n,e):
@@ -33,14 +35,16 @@ def make_polyfit(x,y,n,e):
     return [x_, y_]
 
 class PumpPlot:
-    def __init__(self, name):
+    def __init__(self, name, units=units_US, show_test_dots=True, efficiency_colorbar=False):
         self.name = name
         self.pump_conditions = []
-        self.units = units_US
-        
+        self.units = units
+        self.show_test_dots = show_test_dots
+        self.efficiency_colorbar = efficiency_colorbar
+
     def add_condition(self, condition):
         self.pump_conditions.append(condition)
-        
+
     def all_curves(self, title, save_location):
         plt.figure()
         fig, ax = plt.subplots(3)
@@ -49,23 +53,29 @@ class PumpPlot:
         fig.set_size_inches(8.5, 11)
         for b in self.pump_conditions:
             a = b.ready_curve_for_plotting()
-            q_, e_ = make_polyfit(a['q'], a['e'], 1000, 4)
-            h_ = make_polyfit(a['q'], a['h'], 1000, 4)[1]
-            N_ = make_polyfit(a['q'], a['N'], 1000, 4)[1]
-            ax[0].scatter(q_,h_, c=cm.RdYlGn(np.abs(e_**(3/2))), edgecolor='none')
-            # ax[0].plot(q_, h_)
-            ax[0].scatter(a['q'], a['h'])
+            q_ = ((make_polyfit(a['q'], a['h'], 1000, 4)[0])*a['q'].units).to(self.units['q'])
+            e_ = ((make_polyfit(a['q'], a['e'], 1000, 4)[1])*a['e'].units).to(self.units['e'])
+            h_ = ((make_polyfit(a['q'], a['h'], 1000, 4)[1])*a['h'].units).to(self.units['h'])
+            N_ = ((make_polyfit(a['q'], a['N'], 1000, 4)[1])*a['N'].units).to(self.units['N'])
+            if self.efficiency_colorbar:
+                ax[0].scatter(q_,h_, c=cm.RdYlGn(np.abs(e_**(3/2))), edgecolor='none')
+            else:
+                ax[0].plot(q_, h_)
+            if self.show_test_dots:
+                ax[0].scatter(a['q'].to(self.units['q']), a['h'].to(self.units['h']))
             if 'h' in b.duty_point.keys():
-                ax[0].scatter([b.duty_point['q']],[b.duty_point['h']], s=[1000], marker="+", c="black")
+                ax[0].scatter([(b.duty_point['q']).to(self.units['q'])],[(b.duty_point['h']).to(self.units['h'])], s=[1000], marker="+", c="black")
             ax[1].plot(q_, e_)
-            ax[1].scatter(a['q'], a['e'])
+            if self.show_test_dots:
+                ax[1].scatter(a['q'].to(self.units['q']), a['e'].to(self.units['e']))
             if 'e' in b.duty_point.keys():
-                ax[1].scatter([b.duty_point['q']],[b.duty_point['e']], s=[1000], marker="+", c="black")    
+                ax[1].scatter([b.duty_point['q'].to(self.units['q'])],[b.duty_point['e'].to(self.units['e'])], s=[1000], marker="+", c="black")
             ax[2].plot(q_, N_)
-            ax[2].scatter(a['q'], a['N'])
+            if self.show_test_dots:
+                ax[2].scatter(a['q'].to(self.units['q']), a['N'].to(self.units['N']))
             if 'N' in b.duty_point.keys():
-                ax[2].scatter([b.duty_point['q']],[b.duty_point['N']], s=[1000], marker="+", c="black")
-    
+                ax[2].scatter([b.duty_point['q'].to(self.units['q'])],[b.duty_point['N'].to(self.units['N'])], s=[1000], marker="+", c="black")
+
         ylbl = ax[0].yaxis.get_label()
         ax[0].set_ylabel(unit_mapper[ylbl.get_text()])
         ax[0].set_xlabel('')
@@ -93,30 +103,30 @@ class PumpPlot:
         fig.tight_layout(pad=3)
         fig.savefig(save_location, dpi=300)
 
-          
-    def h_curve(self, title, save_location):
-        plt.figure()
-        fig, ax = plt.subplots(1)
-        fig.suptitle(title)
-        fig.subplots_adjust(hspace=0.1)
-        fig.set_size_inches(11, 8.5)
-        for b in self.pump_conditions:
-            a = b.ready_curve_for_plotting()
-            q_, e_ = make_polyfit(a['q'], a['e'], 1000, 4)
-            h_ = make_polyfit(a['q'], a['h'], 1000, 4)[1]
-            N_ = make_polyfit(a['q'], a['N'], 1000, 4)[1]
-            ax.scatter(q_,h_, c=cm.RdYlGn(np.abs(e_**(3/2))), edgecolor='none')
-            # ax[0].plot(q_, h_)
-            ax.scatter(a['q'], a['h'])
-            if 'h' in b.duty_point.keys():
-                ax.scatter([b.duty_point['q']],[b.duty_point['h']], s=[1000], marker="+", c="black")
 
-        ylbl = ax.yaxis.get_label()
-        ax.set_ylabel(unit_mapper[ylbl.get_text()])
-        # ax.set_xlabel('')
-        ax.grid(True)
-        ax.minorticks_on()
-        ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+    # def h_curve(self, title, save_location):
+    #     plt.figure()
+    #     fig, ax = plt.subplots(1)
+    #     fig.suptitle(title)
+    #     fig.subplots_adjust(hspace=0.1)
+    #     fig.set_size_inches(11, 8.5)
+    #     for b in self.pump_conditions:
+    #         a = b.ready_curve_for_plotting()
+    #         q_, e_ = make_polyfit(a['q'], a['e'], 1000, 4)
+    #         h_ = make_polyfit(a['q'], a['h'], 1000, 4)[1]
+    #         N_ = make_polyfit(a['q'], a['N'], 1000, 4)[1]
+    #         ax.scatter(q_,h_, c=cm.RdYlGn(np.abs(e_**(3/2))), edgecolor='none')
+    #         # ax[0].plot(q_, h_)
+    #         ax.scatter(a['q'], a['h'])
+    #         if 'h' in b.duty_point.keys():
+    #             ax.scatter([b.duty_point['q']],[b.duty_point['h']], s=[1000], marker="+", c="black")
 
-        fig.tight_layout(pad=3)
-        fig.savefig(save_location, dpi=300)
+    #     ylbl = ax.yaxis.get_label()
+    #     ax.set_ylabel(unit_mapper[ylbl.get_text()])
+    #     # ax.set_xlabel('')
+    #     ax.grid(True)
+    #     ax.minorticks_on()
+    #     ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+
+    #     fig.tight_layout(pad=3)
+    #     fig.savefig(save_location, dpi=300)
